bd0d8a2b3b0d47191ed36d94507de92b946b7044
Petro Khovrashchuk <petrokhovrashchuk@gmail.com>
2026-04-18 12:55:16 +0200

    fix backend impl

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,13 @@
 from functools import lru_cache
 from typing import List
 
-from pydantic import BaseSettings, Field, HttpUrl
+from pydantic import Field, HttpUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
     dev_origin: HttpUrl = Field("http://localhost:3000", env="DEV_ORIGIN")
     chrome_extension_id: str = Field(..., env="CHROME_EXTENSION_ID")
 
